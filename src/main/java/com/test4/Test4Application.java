fa0d9bb59968bc4dc6bab8a9fe56a887daae2369
nikhil6225 <113876320+nikhil6225@users.noreply.github.com>
2025-08-18 11:44:11 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 100;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
